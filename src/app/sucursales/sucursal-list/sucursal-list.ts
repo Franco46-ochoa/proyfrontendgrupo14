5e1e4a5bd9e862163d9cfc420bae9cee0d91b409
Leonel <leonelavendano13@gmail.com>
2026-06-26 14:49:04 -0300

    agregar listado de sucursales con datos dummy

--- src/app/sucursales/sucursal-list/sucursal-list.ts
+++ src/app/sucursales/sucursal-list/sucursal-list.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { Sucursal } from '../sucursal.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sucursal-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sucursal-list.html',
   styleUrl: './sucursal-list.scss',
 })
 export class SucursalList {
-
+  sucursales: Sucursal[] = [
+    { id: 1, nombre: 'Sucursal Centro', direccion: 'Belgrano 123', latitud: -24.1858, longitud: -65.2995, telefono: '388-4001122', zonaId: 1 },
+    { id: 2, nombre: 'Sucursal Norte', direccion: 'Av. Bolivia 456', latitud: -24.1700, longitud: -65.3100, telefono: '388-4003344', zonaId: 2 },
+    { id: 3, nombre: 'Sucursal Sur', direccion: 'Ruta 9 km 4', latitud: -24.2100, longitud: -65.2900, telefono: '388-4005566', zonaId: 1 }
+  ];
 }
