82c9b67122754bb85a02f1427189dd96aced4185
Brian Adrian Peña Rodriguez <39559796@fi.unju.edu.ar>
2026-07-07 20:39:17 -0300

    Revert "remove: Google OAuth authentication (login, register, auth service, env config)"
    This reverts commit 9b49bbaa572f9662633fa491789fdffcbdedeb1e.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://smartmargin-api.onrender.com/api'
+  apiUrl: 'https://smartmargin-api.onrender.com/api',
+  googleClientId: '30701075928-nquedma3sqrj6tfr2ln2koodkam376bh.apps.googleusercontent.com'
 };
